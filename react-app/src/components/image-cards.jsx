36b6fd7c2f63b7263367f51958517126cebebb6c
mofopeadegoke <mofopeadegoke@gmail.com>
2023-10-19 10:07:09 +0300

    Home page progress

--- react-app/src/components/image-cards.jsx
+++ react-app/src/components/image-cards.jsx
@@ -1,7 +1,8 @@
+import "../components/styles/image-cards.css";
 export default function ImgCards(props) {
     return (
         <>
-            <article className="img-card" style={{backgroundImage: props.imgUrl}}>
+            <article className="img-card" style={{backgroundImage: "../assests/home-section-two-cardImg-one.jpg"}}>
                 <article className="content">
                     <p className="curvy-text white-text">
                         {props.curvyText}
--- react-app/src/pages/home.jsx
+++ react-app/src/pages/home.jsx
@@ -1,8 +1,8 @@
 import "../styles/home.css";
 import PrimaryColoredBtn from "../components/primary-colored-btn";
-import cardImgOne from "../assests/home-section-two-cardImg-one.jpg";
-import cardImgTwo from "../assests/home-section-two-cardImg-two.jpg";
-import ImgCards from "../components/image-cards";
+import Basket from "../assests/basket.png";
+import FoodBowl from "../assests/vegan.png";
+import MailBox from "../assests/mailbox.png";
 export default function HomePage() {
     return (
         <>
@@ -17,8 +17,51 @@ export default function HomePage() {
                     </article>
                 </article>
                 <article className="section-two">
-                    <ImgCards imgUrl={cardImgOne} curvyText="Natural!!" mainCardTxt="Get Garden Fresh Fruits"/>
-                    <ImgCards imgUrl={cardImgTwo} curvyText="Natural!!" mainCardTxt="Get Garden Fresh Fruits"/>
+                    <article className="img-card one">
+                        <article className="content">
+                            <p className="curvy-text white-text">
+                                Natural!!
+                            </p>
+                            <h2>Get Garden Fresh Fruits</h2>
+                        </article>
+                    </article>
+                    <article className="img-card two">
+                        <article className="content">
+                            <p className="curvy-text white-text">
+                                Offer!!
+                            </p>
+                            <h2>Get 10% off on Vegetables</h2>
+                        </article>
+                    </article>
+                </article>
+                <article className="section-three">
+                    <img src={Basket} alt="A basket of fruits" />
+                    <article className="content">
+                        <p className="curvy-text green-text">
+                            About Us
+                        </p>
+                        <h2>We Believe in Working Accredited Farmers</h2>
+                        <p>Simply dummy text of the printing and typesetting industry. Lorem had ceased to been the industry's standard dummy text ever since the 1500s, when an unknownprinter took a galley.</p>
+                        <article className="row">
+                            <article className="img-container">
+                                <img src={FoodBowl} alt="A food bowl icon" />
+                            </article>
+                            <article className="content">
+                                <h3>Organic Foods Only</h3>
+                                <p>Simply dummy text of the printing and typesetting industry. Lorem Ipsum</p>
+                            </article>
+                        </article>
+                        <article className="row">
+                            <article className="img-container">
+                                <img src={MailBox} alt="A food bowl icon" />
+                            </article>
+                            <article className="content">
+                                <h3>Quality Standards</h3>
+                                <p>Simply dummy text of the printing and typesetting industry. Lorem Ipsum</p>
+                            </article>
+                        </article>
+                        <PrimaryColoredBtn value="Shop Now"/>
+                    </article>
                 </article>
             </article>
         </>
